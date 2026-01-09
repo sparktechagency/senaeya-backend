b85686577e34474d3a20e8894568c0d7e1322a6d
asifaowadud <asifaowadud@gmail.com>
2026-01-09 16:20:05 +0600

    mend

--- src/app/modules/report/report.controller.ts
+++ src/app/modules/report/report.controller.ts
@@ -6,7 +6,10 @@ import { reportService } from './report.service';
 const getAllReportsByCreatedDateRange = catchAsync(async (req: Request, res: Response) => {
      const tokenWithBearer = req.headers.authorization;
      const access_token = tokenWithBearer!.split(' ')[1];
-     const { providerWorkShopId } = req.body;
+     let { providerWorkShopId } = req.body;
+     if (!providerWorkShopId) {
+          providerWorkShopId = req.query.providerWorkShopId as string;
+     }
      const result = await reportService.getAllReportsByCreatedDateRange(req.query, providerWorkShopId, req.user, access_token);
 
      sendResponse(res, {
