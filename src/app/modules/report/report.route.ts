e9de332fd259e24ca4613720cd10bca9e00f649e
asifaowadud <asifaowadud@gmail.com>
2026-01-09 15:39:59 +0600

    mend

--- src/app/modules/report/report.route.ts
+++ src/app/modules/report/report.route.ts
@@ -8,7 +8,13 @@ import { reportValidation } from './report.validation';
 
 const router = express.Router();
 
-router.get('/', auth(USER_ROLES.WORKSHOP_MEMBER, USER_ROLES.WORKSHOP_OWNER),validateRequest(reportValidation.getAllReportsByCreatedDateRangeZodSchema), validateUserAuthority(), reportController.getAllReportsByCreatedDateRange);
-router.get('/dashboard', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN),reportController.getDashboardReport);
+router.get(
+     '/',
+     auth(USER_ROLES.WORKSHOP_MEMBER, USER_ROLES.WORKSHOP_OWNER),
+     validateRequest(reportValidation.getAllReportsByCreatedDateRangeZodSchema),
+     // validateUserAuthority(),
+     reportController.getAllReportsByCreatedDateRange,
+);
+router.get('/dashboard', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), reportController.getDashboardReport);
 
 export const reportRoutes = router;
