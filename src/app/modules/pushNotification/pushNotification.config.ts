f905dd4cb1dd792ddea17af56cf53013a363106c
asifaowadud <asifaowadud@gmail.com>
2026-01-19 12:31:27 +0600

    fix: old push notifications removed

--- src/app/modules/pushNotification/pushNotification.config.ts
+++ src/app/modules/pushNotification/pushNotification.config.ts
@@ -1,16 +1,35 @@
-import admin from 'firebase-admin';
-import config from '../../../config';
-import * as fs from 'fs';
-import * as path from 'path';
+// import admin from 'firebase-admin';
+// import config from '../../../config';
+// import * as fs from 'fs';
+// import * as path from 'path';
 
-// Read the service account file
-const serviceAccountPath = path.resolve(process.cwd(), config.firebase_service_account!);
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+// // Read the service account file
+// const serviceAccountPath = path.resolve(process.cwd(), config.firebase_service_account!);
+// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//   });
+// }
+
+// export const messaging = admin.messaging();
+
+import admin from 'firebase-admin';
+import fs from 'fs';
+import path from 'path';
+import config from '../../../config';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  });
+     const serviceAccountPath = path.resolve(process.cwd(), config.firebase_service_account!);
+
+     const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+
+     admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+     });
 }
+
+export default admin;
 
 export const messaging = admin.messaging();
