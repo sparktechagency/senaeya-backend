c7ea3ed2d80a65336495ec51c8e9fe691c989e59
asifaowadud <asifaowadud@gmail.com>
2026-01-19 12:21:58 +0600

    mend

--- src/app/modules/pushNotification/pushNotification.route.ts
+++ src/app/modules/pushNotification/pushNotification.route.ts
@@ -6,26 +6,20 @@
 // const router = express.Router();
 
 // router.post(
-//   '/send',validateRequest(PushNotificationZodSchema),   
+//   '/send',validateRequest(PushNotificationZodSchema),
 //   PushNotificationControllers.sendPushNotificationController,
 // );
 
 // export const PushNotificationRoutes = router;
 
-
 import express from 'express';
 import { PushNotificationControllers } from './pushNotification.controller';
-import { USER_ROLES } from '../user/user.enums';
 import auth from '../../middleware/auth';
 import validateRequest from '../../middleware/validateRequest';
 import { PushNotificationZodSchema } from './pushNotification.validation';
+import { USER_ROLES } from '../../../enums/user';
 const router = express.Router();
 
-router.post(
-    '/send',
-    auth(USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN),
-    validateRequest(PushNotificationZodSchema),
-    PushNotificationControllers.sendPushNotificationController,
-);
+router.post('/send', auth(USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN), validateRequest(PushNotificationZodSchema), PushNotificationControllers.sendPushNotificationController);
 
 export const PushNotificationRoutes = router;
