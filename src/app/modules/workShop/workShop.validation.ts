b820d0ce5d48c38f386e621e57bf078295afaebe
IsmailHossen87 <ismailhosen8757@gmail.com>
2026-02-17 11:05:56 +0600

    bankAccountNumberOptional

--- src/app/modules/workShop/workShop.validation.ts
+++ src/app/modules/workShop/workShop.validation.ts
@@ -84,13 +84,13 @@ const updateWorkShopZodSchema = z.object({
                .string()
                .trim()
                .regex(/^3\d{14}$/i, 'taxVatNumber must be 15 digits starting with 3')
-               .optional(),
+               .optional().optional(),
           bankAccountNumber: z
                .string()
                .trim()
                .regex(/^SA[A-Z0-9]{22}$/i, 'bankAccountNumber must be 24 characters starting with SA')
                .optional()
-               .nullable(),
+               .nullable().optional(),
           isAvailableMobileWorkshop: z.boolean().optional(),
           workshopGEOlocation: geoLocationSchema.optional(),
           regularWorkingSchedule: workingSubScheduleSchema.partial().optional(),
